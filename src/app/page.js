e02fea8a27315f8c137336350378642320905599
Theresa Summa <theresasumma@gmail.com>
2024-05-27 22:42:21 -0700

    fixing text overlapping on mobile

--- src/app/page.js
+++ src/app/page.js
@@ -94,8 +94,8 @@ function Home() {
               <h5 className="title pink text-2xl mt-2">Lead Innovation Specialist</h5>
             </div>
             <div className="col-span-4">
-              <div className="location text-xl light-gray">Washington, DC and remote</div>
-              <div className="dates text-lg mt-3">December 2014 - August 2018</div>
+              <div className="location text-lg smartphone:text-xl light-gray">Washington, DC and remote</div>
+              <div className="dates text-md smartphone:text-lg mt-3">December 2014 - August 2018</div>
             </div>
             <ul className="responsibilities list-disc list-inside col-span-8 mt-4 text-lg mb-4">
                 <li className="mb-1">Extended software used to parse and view federal regulation for the <Link href="https://regulations.atf.gov/" className="rounded">Alcohol, Tobacco and Firearms Bureau</Link>, increasing regulatory compliance and ease of use for ATF staff using <Link href="https://react.dev/" className="rounded">React</Link>, <Link href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" className="rounded">JavaScript</Link>, <Link className="rounded" href="https://backbonejs.org/">Backbone.js</Link>, <Link href="https://www.python.org/">Python</Link> and <Link href="https://www.djangoproject.com/">Django</Link>, <Link href="https://sass-lang.com/">Sass</Link>, <Link href="https://developer.mozilla.org/en-US/docs/Web/HTML">HTML</Link>.</li>
@@ -109,8 +109,8 @@ function Home() {
               <h5 className="title pink text-2xl mt-2">Innovation Specialist</h5>
             </div>
             <div className="col-span-4">
-              <div className="location text-xl light-gray">Washington, DC and remote</div>
-              <div className="dates text-lg mt-3">August 2014 - December 2014</div>
+              <div className="location text-lg smartphone:text-xl light-gray">Washington, DC and remote</div>
+              <div className="dates text-md smartphone:text-lg mt-3">August 2014 - December 2014</div>
             </div>
             <ul className="responsibilities list-disc list-inside col-span-8 mt-4 text-lg">
               <li>Led continued development of an <Link href="https://buy.gsa.gov/pricing/">internal tool for federal procurement officers</Link> to receive fairer prices on government contracts by comparing past prices on similar contracts, which saved compliance officers time and money on new contracts, using <Link href="https://www.python.org/">Python</Link> and <Link href="https://www.djangoproject.com/">Django</Link>.</li>
@@ -122,8 +122,8 @@ function Home() {
               <h5 className="title pink text-2xl mt-2">Technology & Innovation Fellow</h5>
             </div>
             <div className="col-span-4">
-              <div className="location text-xl light-gray">Washington, DC and remote</div>
-              <div className="dates text-lg mt-3">December 2012 - August 2014</div>
+              <div className="location text-lg smartphone:text-xl light-gray">Washington, DC and remote</div>
+              <div className="dates text-md smartphone:text-lg mt-3">December 2012 - August 2014</div>
             </div>
             <ul className="responsibilities list-disc list-inside col-span-8 mt-4 text-lg">
               <li>Led front end development for innovative and modern <Link href="https://www.consumerfinance.gov/rules-policy/regulations/">software that dynamically parses and displays federal regulations</Link>, revolutionizing the space for federal employees and private industry alike using <Link href="https://www.python.org/">Python</Link>, <Link href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">JavaScript</Link>, <Link href="https://www.djangoproject.com/">Django</Link>, <Link href="https://backbonejs.org/">Backbone.js</Link>, <Link href="https://underscorejs.org/">Underscore.js</Link>, <Link href="https://www.travis-ci.com/">Travis CI</Link>, <Link href="https://saucelabs.com/">Sauce Labs</Link>, <Link href="https://www.selenium.dev/">Selenium</Link>, <Link href="https://mochajs.org/">Mocha</Link>, <Link href="https://gruntjs.com/">Grunt</Link>, shell scripting, <Link href="https://eslint.org/">ESLint</Link>, <Link href="https://babeljs.io/">Babel</Link>, <Link href="https://developer.mozilla.org/en-US/docs/Web/HTML">HTML</Link>, <Link href="https://sass-lang.com/">Sass</Link>.</li>
@@ -143,8 +143,8 @@ function Home() {
               <h5 className="title pink text-2xl mt-2">Web Application Engineer</h5>
             </div>
             <div className="col-span-4">
-              <div className="location text-xl light-gray">New York, NY</div>
-              <div className="dates text-lg mt-3">July 2012 - October 2012</div>
+              <div className="location text-lg smartphone:text-xl light-gray">New York, NY</div>
+              <div className="dates text-md smartphone:text-lg mt-3">July 2012 - October 2012</div>
             </div>
             <ul className="responsibilities list-disc list-inside col-span-8 mt-4 text-lg">
               <li className="mb-1">Led front end engineering for the initial deployment of an innovative news website owned by <Link href="https://www.theatlantic.com/">The Atlantic Media Company</Link>, using <Link href="https://backbonejs.org/">Backbone.js</Link>, <Link href="https://underscorejs.org/">Underscore.js</Link>, <Link href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">JavaScript</Link>, <Link href="https://www.php.net/">PHP</Link>, <Link href="https://wordpress.com/">Wordpress</Link>, <Link href="https://jquery.com/">jQuery</Link>.</li>
@@ -156,8 +156,8 @@ function Home() {
               <h5 className="title pink text-2xl mt-2">Senior Web Developer</h5>
             </div>
             <div className="col-span-4">
-              <div className="location text-xl light-gray">New York, NY</div>
-              <div className="dates text-lg mt-3">May 2010 - April 2012</div>
+              <div className="location text-lg smartphone:text-xl light-gray">New York, NY</div>
+              <div className="dates text-md smartphone:text-lg mt-3">May 2010 - April 2012</div>
             </div>
             <ul className="responsibilities list-disc list-inside col-span-8 mt-4 text-lg mb-4">
               <li className="mb-1">Maintained and built new features for a consumer product comparison website owned by <Link href="https://www.nytco.com/">The New York Times Company</Link>, using <Link href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">JavaScript</Link>, <Link href="https://developer.mozilla.org/en-US/docs/Web/CSS">CSS</Link>, <Link href="https://developer.mozilla.org/en-US/docs/Web/HTML">HTML</Link>, <Link href="https://www.drupal.org/">Drupal</Link>, <Link href="https://www.php.net/">PHP</Link>, <Link href="https://jquery.com/">jQuery</Link>.</li>
@@ -171,8 +171,8 @@ function Home() {
               <h5 className="title pink text-2xl mt-2">Client Side Developer</h5>
             </div>
             <div className="col-span-4">
-              <div className="location text-xl light-gray">New York, NY</div>
-              <div className="dates text-lg mt-3">May 2009 - May 2010</div>
+              <div className="location text-lg smartphone:text-xl light-gray">New York, NY</div>
+              <div className="dates text-md smartphone:text-lg mt-3">May 2009 - May 2010</div>
             </div>
             <ul className="responsibilities list-disc list-inside col-span-8 mt-4 text-lg mb-4">
               <li className="mb-1">Ported complex news site from Coldfusion to <Link href="https://www.drupal.org/">Drupal</Link>.</li>
@@ -185,8 +185,8 @@ function Home() {
               <h5 className="title pink text-2xl mt-2">Drupal Themer</h5>
             </div>
             <div className="col-span-4">
-              <div className="location text-xl light-gray">New York, NY</div>
-              <div className="dates text-lg mt-3">April 2009 - May 2009</div>
+              <div className="location text-lg smartphone:text-xl light-gray">New York, NY</div>
+              <div className="dates text-md smartphone:text-lg mt-3">April 2009 - May 2009</div>
             </div>
             <ul className="responsibilities list-disc list-inside col-span-8 mt-4 text-lg mb-4">
               <li className="mb-1">Ported musician websites from <Link href="https://www.drupal.org/">Drupal</Link> 5 to <Link href="https://www.drupal.org/">Drupal</Link> 6, including artists like Ben Folds and Kesha.</li>
@@ -198,8 +198,8 @@ function Home() {
               <h5 className="title pink text-2xl mt-2">Front End Developer</h5>
             </div>
             <div className="col-span-4">
-              <div className="location text-xl light-gray">New York, NY</div>
-              <div className="dates text-lg mt-3">October 2008 - April 2009</div>
+              <div className="location text-lg smartphone:text-xl light-gray">New York, NY</div>
+              <div className="dates text-md smartphone:text-lg mt-3">October 2008 - April 2009</div>
             </div>
             <ul className="responsibilities list-disc list-inside col-span-8 mt-4 text-lg mb-4">
               <li className="mb-1">Built the front end of two versions of a high traffic media website using <Link href="https://www.drupal.org/">Drupal</Link>, <Link href="https://www.php.net/">PHP</Link>, <Link href="https://developer.mozilla.org/en-US/docs/Web/HTML">HTML</Link>, <Link href="https://developer.mozilla.org/en-US/docs/Web/CSS">CSS</Link>, <Link href="https://subversion.apache.org/">SVN</Link>.</li>
