bd98b5c7df1156a1c7e34a0de20b1c1adb91e513
theresa <theresasumma@gmail.com>
2025-09-16 11:45:04 -0700

    Add custom title and sparkle emoji favicon to Sparklefall page

---
+++ src/app/(standalone)/sparklefall/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Sparklefall JavaScript Library',
+  description: 'Beautiful, customizable falling sparkle animations with zero dependencies',
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">✨</text></svg>',
+  },
+};
+
+export default function SparklefallLayout({ children }) {
+  return children;
+}
