89231cf055c62ae95ede7d0e17b67f2e768bcf1b
theresa <theresasumma@gmail.com>
2026-02-27 12:54:57 -0800

    updating content

--- src/app/page.js
+++ src/app/page.js
@@ -13,8 +13,9 @@ async function Home() {
       <section className="section-light">
         <div className="section-content">
           <strong>Hi.</strong><br/>
-          <h2>My name is Theresa Summa.</h2>
-          <h3>I am a software engineer.</h3>
+          <h2>This is my software engineering blog and resume.</h2>
+          <h3>I have been a software engineer professionally since 2007.</h3>
+            <p>Here you will find information about my personal projects and professional experience.</p>
         </div>
       </section>
 
@@ -28,9 +29,10 @@ async function Home() {
         <div className="section-content">
           <h1>Projects</h1>
           <nav className="project-nav">
-            <li><a href="https://vibecheckai.pro">VibeCheckAI</a></li>
+              <li><a href="https://glossysites.live">Glossy Sites/GlossyCMS</a></li>
             <li><a href="https://theresasumma.com/sparklefall">Sparklefall</a></li>
-            <li><a href="https://lastfriends.site">LastFriends</a></li>
+              <li><a href="https://vibecheckai.pro">VibeCheckAI (no longer maintained)</a></li>
+              <li><a href="https://lastfriends.site">LastFriends</a> (no longer maintained)</li>
             <li><a href="https://github.com/theresaanna">See also: my Github</a></li>
           </nav>
         </div>
@@ -132,7 +134,7 @@ housing crisis off the ground technologically.</p>
           About Theresa and this site:
           <ul>
             <li>This site is built with <a href="https://nextjs.org/">Next.js</a> and hosted with <a href="https://vercel.com/">Vercel</a>.</li>
-            <li>On <a href="https://en.wikipedia.org/wiki/MacOS">MacOS</a>, I use <a href="https://www.warp.dev/">Warp</a> for an AI-powered Terminal replacement. Warp was handy when I botched my Git history and when I ran into dependency versioning issues. I am using <a href="https://www.jetbrains.com/">JetBrains IDEs, PyCharm mostly</a>. I also use <a href="https://claude.ai">Claude</a> as a pair programming partner.</li>
+            <li>On <a href="https://en.wikipedia.org/wiki/MacOS">MacOS</a>, I use <a href="https://www.jetbrains.com/">JetBrains IDEs, WebStorm or PyCharm</a>. I also use <a href="https://claude.ai">Claude</a> as a pair programming partner.</li>
             <li>Theresa is a crafter, thinker, cat mom, Lady Gaga superfan, lifelong tinkerer.</li>
           </ul>
         </div>
